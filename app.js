4562eb94df153f220561ad618453e193cde58509
Chris Joo <chrisj1225@gmail.com>
2023-01-17 00:23:44 -0500

    add test express routing for user api

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-const port = process.env.PORT || 3000;
-
 app.get("/", (req, res) => res.send("Hello World! My name is Chris"));
+app.use("/api/users", users);
+  
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
