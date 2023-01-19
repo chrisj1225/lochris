e22fab4730dcc0391e28bd1055c3011f248562b7
Chris Joo <chrisj1225@gmail.com>
2023-01-19 13:24:31 -0500

    fix user login error obj

--- routes/api/users.js
+++ routes/api/users.js
@@ -90,7 +90,7 @@ router.post('/login', (req, res) => {
   User.findOne({ email })
     .then(user => {
       if (!user) {
-        errors.handle = "This user does not exist";
+        errors.email = "This user does not exist";
         return res.status(404).json(errors);
       }
 
