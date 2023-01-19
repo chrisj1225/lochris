1203b60b58a02a8de69c6acfad0173b7ace17cac
Chris Joo <chrisj1225@gmail.com>
2023-01-19 16:51:57 -0500

    set up basic frontend

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.use(passport.initialize());
 passportFunc(passport);
 app.use("/api/users", users);
   
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
