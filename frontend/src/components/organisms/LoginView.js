d597a921b219d5d3316a395a086158cedbe86133
Chris Joo <chrisj1225@gmail.com>
2023-01-20 20:42:03 -0500

    Create basic LoginView

--- frontend/src/components/organisms/LoginView.js
+++ frontend/src/components/organisms/LoginView.js
@@ -1,10 +1,71 @@
 import React from 'react';
 
+import useSessions from '../../hooks/useSessions.hooks';
+import useSessionErrors from '../../hooks/useSessionErrors.hooks';
+
 const LoginView = () => {
+  const {
+    isAuthenticated,
+    user,
+    isLoggedIn,
+    loginUser,
+  } = useSessions();
+  const { errors } = useSessionErrors();
+
+  const [state, setState] = React.useState({
+    email: '',
+    password: '',
+    errors: {},
+  });
+
+  const updateField = field => {
+    return e => setState({
+      ...state,
+      [field]: e.currentTarget.value,
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    let user = {
+      email: state.email,
+      password: state.password,
+    }
+
+    loginUser(user);
+  }
+
+  const renderErrors = () => {
+    <ul>
+      {Object.keys(errors).map((errKey, i) => (
+        <li key={i}>
+          {errors[errKey]}
+        </li>
+      ))}
+    </ul>
+  }
 
   return (
     <div>
-      <h1>This is the Login page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text"
+            value={state.email}
+            onChange={updateField('email')}
+            placeholder="Email"
+          />
+          <input type="password"
+            value={state.password}
+            onChange={updateField('password')}
+            placeholder="password"
+          />
+          <input type="submit"
+            value="Submit"
+          />
+          {renderErrors()}
+        </div>
+      </form>
     </div>
   )
 }
