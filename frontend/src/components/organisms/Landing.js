bfc9c6d288464d33ec9cd495ebf3719d559f4226
Chris Joo <chrisj1225@gmail.com>
2023-01-21 03:07:48 -0500

    Add styled-components and test

--- frontend/src/components/organisms/Landing.js
+++ frontend/src/components/organisms/Landing.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Landing = () => {
 
   return (
-    <div>
-      <h1>This is the landing page</h1>
-    </div>
+    <Wrapper>
+      <Title>This is the landing page</Title>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background: gray;
+`;
+
+const Title = styled.h1`
+  color: lightblue;
+  margin: 0;
+`;
 
 export default Landing;
