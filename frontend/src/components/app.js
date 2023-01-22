098b56621f14bbaf586574aac717387a98fde01a
Chris Joo <chrisj1225@gmail.com>
2023-01-21 23:13:48 -0500

    Create App.css and add #root styling

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
+import '../App.css';
+
 import NavBar from './molecules/NavBar';
 import {
   Landing,
