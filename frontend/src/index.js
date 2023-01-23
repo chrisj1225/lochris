73613755c49d347260e02eeb7b7095c3e2bbe279
Chris Joo <chrisj1225@gmail.com>
2023-01-23 18:48:10 -0500

    Fix import from sessionsApi in entry file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import jwt_decode from 'jwt-decode';
 import './index.css';
 import Root from './components/root';
 import configureStore from './store/store';
-import { setAuthToken } from './util/session_api_util';
+import { setAuthToken } from './util/sessionsApi';
 import { logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
