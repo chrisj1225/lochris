e2c4713dc391061524d863a494e98eaa1f62462b
Chris Joo <chrisj1225@gmail.com>
2023-01-23 19:28:51 -0500

    Update Landing component with useRsvps hook

--- frontend/src/components/organisms/Landing.js
+++ frontend/src/components/organisms/Landing.js
@@ -2,9 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { ContentWrapper, Title } from '../../styles/ViewStyles';
+import useRsvps from '../../hooks/useRsvps.hooks';
+import useSessions from '../../hooks/useSessions.hooks';
 
 const Landing = () => {
+  const { user } = useSessions();
+  const {
+    rsvpFetched,
+    currentRsvp,
+    createRsvp,
+  } = useRsvps(user.id);
 
+  console.log({ user, rsvpFetched, currentRsvp });
   return (
     <ContentWrapper>
       <Title>This is the landing page</Title>
