c6a62505a234a33364d552b9f7b21b6a2d43ae21
Chris Joo <chrisj1225@gmail.com>
2023-02-02 01:15:06 -0500

    Add placeholder photo & text to RegistryView

--- frontend/src/components/organisms/RegistryView.js
+++ frontend/src/components/organisms/RegistryView.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { ContentWrapper, Title } from '../../styles/ViewStyles';
+import { ContentWrapper, GeneralText, Title } from '../../styles/ViewStyles';
 
 const RegistryView = () => {
   const location = useLocation();
 
   return (
     <ContentWrapper path={location.pathname}>
-      <Title>This is the Registry page</Title>
+      <Title>Registry</Title>
+      <GeneralText>Add Photo Here</GeneralText>
+      <RegistryWrapper>
+        <GeneralText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus bibendum sapien, at vestibulum eros. Nam in augue dapibus, lacinia nunc quis, aliquam neque. Vestibulum imperdiet ipsum nec porta tincidunt. Mauris nec quam sed est sodales dignissim. Aliquam erat volutpat. Proin et semper ipsum, et euismod sapien.
+        </GeneralText>
+      </RegistryWrapper>
     </ContentWrapper>
   );
 };
+
+const RegistryWrapper = styled.div`
+  width: 500px;
+`;
 
 export default RegistryView;
